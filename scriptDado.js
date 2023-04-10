c2def7f8735c3bb7c8f6cc2fe00b4b400aa3c748
Blixpoew <129560716+Blixpoew@users.noreply.github.com>
2023-04-10 15:58:39 -0600

    DadoUpdate

---
+++ scriptDado.js
@@ -0,0 +1,40 @@
+let images = ["dice-01.svg",
+"dice-02.svg",
+"dice-03.svg",
+"dice-04.svg",
+"dice-05.svg",
+"dice-06.svg"];
+let dice = document.querySelectorAll("img");
+let flipBtn = document.querySelector(".rollDice");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        console.log(dieOneValue);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#total").innerHTML = "Resultado: " + ( (dieOneValue +1) );
+    },
+    3000
+    );
+	setTimeout(fxConfetti, 3000);
+	disableButton();
+}
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = false;
+    },3000);
+}
+function fxConfetti(){
+	confetti({
+		particleCount: 100,
+		spread: 70,
+		origin: {y: 0.6}
+	})
+}
